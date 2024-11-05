e8f4e133b983b4f6be69b9031351624db07e8f04
sedatsoyan <bekr78@outlook.com>
2024-11-05 17:12:21 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+
+//it's simple code!!!
+// input output
+
+int main() {
+    int a,b;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    printf("Enter a number:");
+    scanf("%d",&b);
+
+
+    float divide = (float) a/b;
+
+    printf("divide =%f",divide);
+
+    return 0;
+
+}
